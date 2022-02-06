9d7498cc9cf7d3f4dec56d12ba1e37e2a516c8f9
Warths <pro.warths@gmail.com>
2022-02-06 20:56:46 +0100

    Declared new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './component/navigation/navigation/navigation.component';
+import { ProfileComponent } from './component/navigation/profile/profile.component';
+import { LoginComponent } from './component/navigation/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    ProfileComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
